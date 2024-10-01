aa2427c3b814ee2cc99ca2481c70d03285ff78a7
harshkalal7 <kalalharsh704@gmail.com>
2024-10-01 16:35:14 +0530

    day 8

--- day7.cpp
+++ day7.cpp
@@ -1,5 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    
-}
+int main()
+{
+    // int x, y = 1;
+    // cout<<"Enter value of x";
+    // cin>>x;
+    // do
+    // {
+    //     cout << x << "*" << y << "=" << x * y << endl;
+    //     y++;
+    // } while (y <= 10);
+
+    // float sum=0;
+    // int arr[] = {2, 2, 2, 5, 24};
+    // for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+    // {
+    //     sum += arr[i];
+    // }
+    // cout<<sum/(sizeof(arr)/sizeof(arr[0]));
+
+    // int temp=0;
+    // int arr[] = {2, 12, 2, 5, 24};
+    // for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+    // {
+    //     continue;
+    //     if (arr[i] > temp){
+    //         temp = arr[i];
+    //     }
+    // }
+    // cout<<temp;
+
+    //     int x = 6;
+    // zero:
+    //   cin>>x;
+    //     // break;
+    //     if (x != 0)
+    //     {
+    //         goto zero;
+    //     }
+
+    //     void first(){
+    //         cout << "helo";
+    //     }
+
+    //     first();
+    // }
---
+++ day8.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+// void first(int *y)
+// {
+//     *y=6;
+//     cout << "helo "<<*y;
+// }
+// int main()
+// {
+//     int x=5;
+//     // int *y = &x;
+//     // *y = 6;
+//     first(&x);
+//     cout<<x;
+// }
+
+// void first(int *x)
+// {
+//     int p;
+//     cin >> p;
+//     *x = p;
+// }
+// int main()
+// {
+//     int i = 0;
+//     int arr[] = {2, 3, 4};
+//     // *x = ;
+//     first(&arr[i]);
+//     for(int j=0;j<3;j++){
+//         cout<<arr[j]<<endl;
+//     }
+// }
+
+// for call by value or refernce
