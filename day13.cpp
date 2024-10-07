197f24b903706a80e4e2f2110e57fb6cb74108af
harshkalal7 <kalalharsh704@gmail.com>
2024-10-07 22:39:35 +0530

    day 13

--- day13.cpp
+++ day13.cpp
@@ -21,13 +21,67 @@ int main()
 //     }
 // }
 
+// {
+//     int i, j;
+//     for (i = 1; i <= 3; i++)
+//     {
+//         for (j = 1; j <= 4 - i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for (int k = 1; k <= 4; k++)
+//         {
+//             if (k <= i)
+//             {
+
+//                 cout << "* ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+// {
+//     int i, j, n;
+//     cin >> n;
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= n; j++)
+//         {
+//            if ( i == 1 || j == 1 || j == n || i == n || i==j || i+j == 9){
+//             cout<<"*";
+//            }
+//            else {
+//             cout<<" ";
+//            }
+//         }
+//         cout<<endl;
+//     }
+// }
+
 {
-    int i, j;
-    for (i = 3; i >= 1; i--)
+    int i, j, n;
+    cin >> n;
+    for (i = 1; i <= n; i++)
     {
-        for (j = 3; j >= 1; j--)
+        for (j = 1; j <= (n * 2) - i; j++)
         {
-            if ((i == 2 || j == 2) && (i == 3 || j == 3))
+            if (i > 1 && j < i)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+    for (i = 1; i < n; i++)
+    {
+        for (j = 1; j <= 2 + 2 * i; j++)
+        {
+            if (j > 1 && i<j)
             {
                 cout << " ";
             }
