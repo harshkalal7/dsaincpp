bea002af3c41e97ca3a9689e6db81d99e5753ea4
harshkalal7 <kalalharsh704@gmail.com>
2024-10-08 16:34:43 +0530

    day 13

--- day13.cpp
+++ day13.cpp
@@ -59,37 +59,105 @@ int main()
 //     }
 // }
 
-{
-    int i, j, n;
-    cin >> n;
-    for (i = 1; i <= n; i++)
-    {
-        for (j = 1; j <= (n * 2) - i; j++)
-        {
-            if (i > 1 && j < i)
-            {
-                cout << " ";
-            }
-            else
-            {
-                cout << "*";
-            }
-        }
-        cout << endl;
-    }
-    for (i = 1; i < n; i++)
-    {
-        for (j = 1; j <= 2 + 2 * i; j++)
-        {
-            if (j > 1 && i<j)
-            {
-                cout << " ";
-            }
-            else
-            {
-                cout << "*";
-            }
-        }
-        cout << endl;
-    }
-}
+// {
+//     int i, j, n;
+//     cin >> n;
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= (n * 2) - i; j++)
+//         {
+//             if (i > 1 && j < i)
+//             {
+//                 cout << " ";
+//             }
+//             else
+//             {
+//                 cout << "*";
+//             }
+//         }
+//         cout << endl;
+//     }
+//     for (i = 1; i < n; i++)
+//     {
+//         for (j = 1; j <= 2 + 2 * i; j++)
+//         {
+//             if (j > 1 && i<j)
+//             {
+//                 cout << " ";
+//             }
+//             else
+//             {
+//                 cout << "*";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+// {
+//     int i, j, n, num = 1;
+//     cin >> n;
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= n; j++)
+//         {
+//             cout << num;
+//             num++;
+//         }
+//         cout << endl;
+//         num = 1;
+//     }
+// }
+
+// {
+//     int i, j, n, num = 1;
+//     cin >> n;
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= n; j++)
+//         {
+//             cout << num;
+//             num++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// {
+//     int i, j, n = 4;
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= n; j++)
+//         {
+//             if (i >= j)
+//             {
+//                 cout << i;
+//             }
+//             else
+//             {
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+// {
+//     int i, j, n = 4;
+//     for (i = 1; i <= n; i++)
+//     {
+//         for (j = 1; j <= n; j++)
+//         {
+//             if (i >= j)
+//             {
+//                 cout << j;
+//             }
+//             else
+//             {
+//                 cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
