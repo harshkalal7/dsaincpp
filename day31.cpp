54669d644d13867a43fcaf3dcf9a9ed07c530267
harshkalal7 <kalalharsh704@gmail.com>
2024-12-05 21:03:38 +0530

    practice

--- day31.cpp
+++ day31.cpp
@@ -69,3 +69,17 @@
 //     return 0;
 // }
 
+//template
+// #include<iostream>
+// using namespace std;
+// template <typename T>
+// T add (T a, T b)
+// {
+//     return a + b;
+// }
+// int main()
+// {
+//     cout<<"Addition of integers:"<<add(3, 7)<<endl;
+//     cout<<"Addition of doubles:"<<add(3.5, 2.1)<<endl;
+//     return 0;
+// }
