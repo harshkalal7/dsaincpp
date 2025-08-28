cdc6b98c69da1ff13152546ee35f365e67a630b2
harshkalal7 <kalalharsh704@gmail.com>
2025-08-28 20:16:01 +0530

    loops and conditional statement

--- java2.java
+++ java2.java
@@ -1,3 +1,23 @@
+import java.util.*;
+
 public class java2 {
+    public static void main(String args[]){
+ Scanner sc = new Scanner(System.in);
+    // int age = sc.nextInt();
+    // if (age>18){
+    //     System.out.println("Adult");
+    // }
+    // else {
+    //         System.out.println("Not Adult");
+    //     }
     
+    int button = sc.nextInt();
+    switch (button){
+        case 1 : System.out.println("hello");
+        break;
+        case 2: System.out.println("namaste");
+        break;
+        case 3 : System.out.println("bonjour");
+    }
+    }
 }
---
+++ java3.java
@@ -0,0 +1,7 @@
+public class java3 {
+ public static void main (String[] args){
+    for (int i = 1; i<=10; i++){
+        System.out.println(i);
+    }
+ }    
+}
